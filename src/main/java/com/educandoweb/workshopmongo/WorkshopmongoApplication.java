2a6dd091fd2e934c0c113155a7ac40d085aaf248
William Ferreira <will.i.am.002@hotmail.com>
2020-09-27 19:59:36 -0300

    Entity User e REST funcionando

--- src/main/java/com/educandoweb/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/educandoweb/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.educandoweb.curso;
+package com.educandoweb.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
