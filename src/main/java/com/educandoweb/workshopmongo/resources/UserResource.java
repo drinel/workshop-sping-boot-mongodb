50a5c89350eff8c43bba4b7f9d1ce5db69123014
William Ferreira <will.i.am.002@hotmail.com>
2020-09-30 11:28:57 -0300

    Obtendo um usuário por id

--- src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
+++ src/main/java/com/educandoweb/workshopmongo/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -29,4 +30,12 @@ public class UserResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 	
+	// pathvariable relacionar id do argumento com id da url 
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id){
+		User obj = service.findById(id);
+
+		return ResponseEntity.ok().body(new UserDTO(obj));
+	}
 }
